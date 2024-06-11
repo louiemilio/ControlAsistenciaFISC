48046233063ae92c247cccae3c97067ff8fb27d9
Kris <akriscastillo@gmail.com>
2024-06-11 10:31:53 -0500

    Agregando la funcionalidad con la base de datos, marcaje y el registro completo de los usuarios en las plantillas

--- src/app/components/employee-times/employee-times.component.ts
+++ src/app/components/employee-times/employee-times.component.ts
@@ -1,82 +1,42 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
-interface UserTimes {
-  fecha: string,
-  nombre: string,
-  apellido: string,
-  cip: string,
-  direccion: string,
-  telefono: string,
-  departamento: string,
-  email: string,
-  estatus: string,
-  entrada: string,
-  sal_alm: string,
-  ent_alm: string,
-  salida: string,
-}
+import { userModel } from '../../shared/users.model';
+import { ListUsersService } from '../../services/list-users.service';
 
 @Component({
   selector: 'app-employee-times',
   standalone: true,
   imports: [NgFor, RouterLink, FormsModule, CommonModule],
   templateUrl: './employee-times.component.html',
-  styleUrl: './employee-times.component.css'
+  styleUrls: ['./employee-times.component.css']
 })
-export class EmployeeTimesComponent {
-  listUsers: UserTimes[] = [];
-  newUser: UserTimes = {
-    fecha: '',
-    nombre: '',
-    apellido: '',
-    cip: '',
-    direccion: '',
-    telefono: '',
-    departamento: '',
-    email: '',
-    estatus: '',
-    entrada: '',
-    sal_alm: '',
-    ent_alm: '',
-    salida: '',
-  };
+export class EmployeeTimesComponent implements OnInit {
+  listUsers: userModel[] = [];
   numEdit: number | null = null;
 
+  constructor(private listUsersService: ListUsersService) {}
 
-  addTimeEmployee() {
-    this.listUsers.push({...this.newUser});
-    this.newUser = {
-      fecha: '',
-      nombre: '',
-      apellido: '',
-      cip: '',
-      direccion: '',
-      telefono: '',
-      departamento: '',
-      email: '',
-      estatus: '',
-      entrada: '',
-      sal_alm: '',
-      ent_alm: '',
-      salida: '',
-    };
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.listUsersService.getUser().subscribe((response: userModel[]) => {
+      this.listUsers = response;
+    });
   }
 
   editTimeEmployee(index: number) {
     this.numEdit = index;
   }
 
-  saveTimeEmployee() {
-    if (this.numEdit !== null) {
-      this.listUsers[this.numEdit] = {...this.listUsers[this.numEdit]};
+  saveTimeEmployee(index: number) {
+    const userToUpdate = this.listUsers[index];
+    this.listUsersService.updateUser(userToUpdate).subscribe(() => {
       this.numEdit = null;
-    }
-  }
-
-  deleteTimeEmployee(index: number) {
-    this.listUsers.splice(index, 1);
+      this.getUsers();
+    });
   }
 }
--- src/app/shared/users.model.ts
+++ src/app/shared/users.model.ts
@@ -1,17 +1,20 @@
+import { Time } from "@angular/common";
+
 export class userModel {
     constructor(
-        public fecha: string,
-        public nombre: string,
-        public apellido: string,
-        public cip: string,
-        public direccion: string,
-        public telefono: string,
-        public departamento: string,
-        public email: string,
-        public estatus: string,
-        public entrada: string,
-        public sal_alm: string,
-        public ent_alm: string,
-        public salida: string,
+      public fecha: Date,
+      public nombre: string,
+      public apellido: string,
+      public cip: string,
+      public direccion: string,
+      public telefono: string,
+      public departamento: string,
+      public email: string,
+      public estatus: string,
+      public entrada: string,
+      public sal_alm: string,
+      public ent_alm: string,
+      public salida: string
     ) {}
-}
+  }
+  
